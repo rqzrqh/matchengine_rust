068f15d052cc38de7a69f790be278a6ff5dce281
george <rqzrqh@163.com>
2025-07-29 22:33:37 +0800

    kafka consumer change to  rust_rdkafka

--- deploy/kafka_market.sh
+++ deploy/kafka_market.sh
@@ -4,11 +4,10 @@ if [ $# -ne 1 ];then
 fi
 
 MARKET=$1
-KAFKA_DIR="/Users/rqzrqh/Public/kafka_2.12-2.8.0/bin/"
 
-$KAFKA_DIR/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic offer.$MARKET
-$KAFKA_DIR/kafka-topics.sh --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic offer.$MARKET
+kafka-topics --bootstrap-server localhost:9092 --delete --topic offer.$MARKET
+kafka-topics --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic offer.$MARKET
 
-$KAFKA_DIR/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic quote_deals.$MARKET
-$KAFKA_DIR//kafka-topics.sh --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic quote_deals.$MARKET
+kafka-topics --bootstrap-server localhost:9092 --delete --topic quote_deals.$MARKET
+kafka-topics --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic quote_deals.$MARKET
 
--- deploy/kafka_settle.sh
+++ deploy/kafka_settle.sh
@@ -1,14 +1,13 @@
 HASH_NUM=64
-KAFKA_DIR="/Users/rqzrqh/Public/kafka_2.12-2.8.0/bin/"
 
 for i in `seq $HASH_NUM`
 do
         INDEX=`expr $i - 1`
-        $KAFKA_DIR/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic settle.$INDEX
+        kafka-topics --bootstrap-server localhost:9092 --delete --topic settle.$INDEX
 done
 
 for i in `seq $HASH_NUM`
 do
         INDEX=`expr $i - 1`
-        $KAFKA_DIR/kafka-topics.sh --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic settle.$INDEX
+        kafka-topics --bootstrap-server localhost:9092 --create --replication-factor 1 --partitions 1 --topic settle.$INDEX
 done
