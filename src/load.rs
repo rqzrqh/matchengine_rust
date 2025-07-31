deddf54195b5350e26e65811466c1b076b22a45c
george <rqzrqh@163.com>
2025-07-31 10:00:36 +0800

    snap add more field and restore correctly

--- src/load.rs
+++ src/load.rs
@@ -1,10 +1,12 @@
 use crate::market::*;
 use mysql::*;
 use mysql::prelude::*;
+use core::panic;
 use std::rc::Rc;
 use std::cell::Cell;
 use rust_decimal::prelude::*;
 use std::process;
+use json::*;
 
 fn load_order(m: &mut Market, conn: &mut PooledConn, timestamp: i64) {
     let table = format!("snap_order_{}", timestamp);
@@ -95,33 +97,136 @@ fn load_order(m: &mut Market, conn: &mut PooledConn, timestamp: i64) {
     }
 }
 
-pub fn restore_state(m: &mut Market, pool: &Pool) {
-
+pub fn restore_output_state(m: &mut Market, pool: &Pool) -> Option<(u64, u64, u64)> {
     let mut conn = pool.get_conn().unwrap();
 
-    let res = conn.query_map(
-        "SELECT `time`, `oper_id`, `order_id`, `deals_id`, `message_id`, `input_offset` from `snap` ORDER BY `id` DESC LIMIT 1", 
-        |(time, oper_id, order_id, deals_id, message_id, input_offset)| (time, oper_id, order_id, deals_id, message_id, input_offset)
-    );
+    let sql = "SELECT `id`, `quote_deals_id`, `settle_message_ids` from `snap` ORDER BY `id` DESC LIMIT 1";
+    let res: Option<(u64, u64, String)> = conn.query_first(&sql).unwrap();
 
     match res {
-        Ok(v) => {
-            if v.len() != 0 {
-                m.oper_id = v[0].1;
-                m.order_id = v[0].2;
-                m.deals_id = v[0].3;
-                m.message_id = v[0].4;
-                m.input_offset = v[0].5;
+        Some(v) => {
 
-                info!("last state time:{} oper_id:{} order_id:{} deals_id:{} message_id:{} mq_offset:{}", 
-                    v[0].0, m.oper_id, m.order_id, m.deals_id, m.message_id, m.input_offset);
+            let id = v.0;
+            let quote_deals_id = v.1;
+            let str_settle_message_ids = v.2;
 
-                load_order(m, &mut conn, v[0].0);
+            let parsed = json::parse(&str_settle_message_ids).expect("json decode failed");
+            info!("{}", parsed);
+
+            if !parsed.is_array() {
+                error!("settle_message_ids is not array {}", parsed);
+                process::exit(0);
             }
+
+            if m.settle_message_ids.len() != parsed.len() {
+                error!("settle message ids length not equal {} {}", m.settle_message_ids.len(), parsed.len());
+                process::exit(0);
+            }
+
+            for i in 0..parsed.len() {
+                let group_message_id = parsed[i].as_u64().unwrap();
+                m.settle_message_ids[i] = group_message_id;
+            }
+
+            // find the first non-zero value
+            let mut min_settle_group_message_id:u64 = 0;
+            let mut settle_message_ids = m.settle_message_ids.clone();
+            settle_message_ids.sort();
+            for i in 0..settle_message_ids.len() {
+                if settle_message_ids[i] != 0 {
+                    min_settle_group_message_id = settle_message_ids[i];
+                    break;
+                }
+            }
+
+            let fake_snap_id = id + 1;
+
+            info!("output state id:{} quote_deals_id:{} min_settle_message_id:{}", 
+                id, quote_deals_id, min_settle_group_message_id);
+
+            return Some((fake_snap_id, quote_deals_id, min_settle_group_message_id));
         },
-        Err(e) => {
-            print!("query failed {}", e);
-            process::exit(0);
+        None => {
+            info!("no output state");
+            return None;
         },
     }
+}
+
+pub fn restore_state(m: &mut Market, pool: &Pool) {
+
+    let mut asks = 0;
+    let mut bids = 0;
+
+    let output_state: Option<(u64, u64, u64)> = restore_output_state(m, pool);
+    match output_state {
+        Some(output) => {
+
+            let mut conn = pool.get_conn().unwrap();
+            let mut last_snap_id = output.0;
+            let quote_deals_id = output.1;
+            let min_settle_group_message_id = output.2;
+
+            loop {
+                let sql = format!("SELECT `id`, `time`, `oper_id`, `order_id`, `deals_id`, `message_id`, `input_offset`, `asks`, `bids` from `snap` WHERE `id` < {} ORDER BY `id` DESC LIMIT 1", last_snap_id);
+                info!("{}", sql);
+                let res: Option<(u64, i64, u64, u64, u64, u64, i64, u32, u32)> = conn.query_first(&sql).unwrap();
+                match res {
+                    Some(v) => {
+
+                        let id = v.0;
+                        let tm = v.1;
+                        let oper_id = v.2;
+                        let order_id = v.3;
+                        let deals_id = v.4;
+                        let message_id = v.5;
+                        let input_offset = v.6;
+                        asks = v.7;
+                        bids = v.8;
+
+                        info!("found snap id:{} time:{} oper_id:{} order_id:{} deals_id:{} message_id:{} input_offset:{} asks:{} bids:{}", 
+                            id, tm, oper_id, order_id, deals_id, message_id, input_offset, asks, bids);
+
+                        if deals_id > quote_deals_id || message_id > min_settle_group_message_id {
+
+                            if deals_id > quote_deals_id {
+                                info!("deals_id not meet condition {} {}", deals_id, quote_deals_id);
+                            }
+
+                            if message_id > min_settle_group_message_id {
+                                info!("message_id not meet condition {} {}", message_id, min_settle_group_message_id);
+                            }
+
+                            last_snap_id = id;
+                            continue;
+                        }
+
+                        info!("deals_id meet condition {} {}", deals_id, quote_deals_id);
+                        info!("message_id meet condition {} {}", message_id, min_settle_group_message_id);
+
+                        m.oper_id = oper_id;
+                        m.order_id = order_id;
+                        m.deals_id = deals_id;
+                        m.message_id = message_id;
+                        m.input_offset = input_offset;
+
+                        load_order(m, &mut conn, tm);
+                    },
+                    None => {
+                        info!("not found snap");
+                    },
+                }
+                break;
+            }
+        },
+        None => {
+        }
+    }
+
+    if asks != m.asks.len() as u32 || bids != m.bids.len() as u32 {
+        panic!("order count error {} {} {} {}", asks, m.asks.len(), bids, m.bids.len());
+    }
+
+    info!("restore state oper_id:{} order_id:{} deals_id:{} message_id:{} input_offset:{}", 
+        m.oper_id, m.order_id, m.deals_id, m.message_id, m.input_offset);
 }
--- src/publish.rs
+++ src/publish.rs
@@ -4,21 +4,35 @@ use std::rc::Rc;
 use rust_decimal::prelude::*;
 use json::*;
 use kafka::producer::{Producer, Record, RequiredAcks};
-
-pub static USER_GROUP_SIZE:u32 = 64;
-
-pub struct PublishInfo {
-    pub topic: String,
-    pub data: JsonValue,
-}
+use crate::task::*;
 
 pub struct Publish {
-    sender: mpsc::Sender<Box<PublishInfo>>,
+    sender: mpsc::Sender<Box<OutputPublishTask>>,
+    settle_group_count:u32,
+}
+
+struct QuotePublishTaskInfo {
+    deals_id:u64,
+    topic:String,
+    data:JsonValue,
+}
+
+struct SettlePublishTaskInfo {
+    group_id:u32,
+    message_id:u64,
+    topic:String,
+    data:JsonValue,
+}
+
+enum OutputPublishTask {
+    QuotePublishTask(QuotePublishTaskInfo),
+    SettlePublishTask(SettlePublishTaskInfo),
 }
 
 impl Publish {
-    pub fn new(brokers: String) -> Publish {
+    pub fn new(brokers: String, main_routine_sender: mpsc::Sender<Task>, quote_deals_id: u64, settle_message_ids: Vec<u64>) -> Publish {
         let (sender, receiver) = mpsc::channel();
+        let settle_group_count = settle_message_ids.len() as u32;
 
         thread::spawn(move || {
             let mut producer = Producer::from_hosts(vec!(brokers.to_owned()))
@@ -26,23 +40,55 @@ impl Publish {
                     .with_required_acks(RequiredAcks::One)
                     .create()
                     .unwrap();
+            let mut quote_deals_id = quote_deals_id;
+            let mut settle_message_ids:Vec<u64> = settle_message_ids.clone();
 
             loop {
-                let received:Box<PublishInfo> = receiver.recv().unwrap();
-                let topic = received.topic;
-                let data = &received.data;
-                producer.send(&Record::from_value(&topic, data.to_string().as_bytes())).unwrap();
+                let task:Box<OutputPublishTask> = receiver.recv().unwrap();
+                let x = task.as_ref();
+                match x {
+                    OutputPublishTask::QuotePublishTask(tsk) => {
+                        if tsk.deals_id > quote_deals_id {
+                            let topic = &tsk.topic;
+                            let data = &tsk.data;
+                            producer.send(&Record::from_value(&topic, data.to_string().as_bytes())).unwrap();
+                            quote_deals_id = tsk.deals_id;
+
+                            let task = PublishProgressTask{
+                                quote_deals_id: quote_deals_id,
+                                settle_message_ids: settle_message_ids.clone(),
+                            };
+                            main_routine_sender.send(Task::ProgressUpdateTask(task)).expect("send progress update task failed");
+                        }
+                    },
+                    OutputPublishTask::SettlePublishTask(tsk) => {
+
+                        let group_id = tsk.group_id as usize;
+
+                        if tsk.message_id > settle_message_ids[group_id] {
+                            let topic = &tsk.topic;
+                            let data = &tsk.data;
+                            producer.send(&Record::from_value(&topic, data.to_string().as_bytes())).unwrap();
+                            settle_message_ids[group_id] = tsk.message_id;
+
+                            let task = PublishProgressTask{
+                                quote_deals_id: quote_deals_id,
+                                settle_message_ids: settle_message_ids.clone(),
+                            };
+                            main_routine_sender.send(Task::ProgressUpdateTask(task)).expect("send progress update task failed");
+                        }
+                    }
+                }
             }
         });
 
         Publish {
             sender: sender,
+            settle_group_count: settle_group_count,
         }
     }
 
-    pub fn publish_put_order(&self, m: &mut Market, extern_id: u64, order: &Rc<Order>) {
-
-        m.message_id += 1;
+    pub fn publish_put_order(&self, m: &Market, extern_id: u64, order: &Rc<Order>) {
 
         let mut object = JsonValue::new_object();
         object["type"] = "put_order".into();
@@ -51,21 +97,22 @@ impl Publish {
         object["txid"] = extern_id.into();
         object["order"] = order.to_json();
 
-        let topic = format!("settle.{}", order.user_id%USER_GROUP_SIZE);
+        let group_id = order.user_id%self.settle_group_count;
+        let topic = format!("settle.{}", group_id);
 
         info!("{} {}", topic, object);
 
-        let message = PublishInfo {
+        let message = SettlePublishTaskInfo {
+            group_id:group_id,
+            message_id:m.message_id,
             topic: topic,
             data: object,
         };
 
-        self.sender.send(Box::new(message)).expect("send failed");
+        self.sender.send(Box::new(OutputPublishTask::SettlePublishTask(message))).expect("send failed");
     }
 
-    pub fn publish_cancel_order(&self, m: &mut Market, extern_id: u64, order: &Rc<Order>) {
-
-        m.message_id += 1;
+    pub fn publish_cancel_order(&self, m: &Market, extern_id: u64, order: &Rc<Order>) {
 
         let mut object = JsonValue::new_object();
         object["type"] = "cancel_order".into();
@@ -74,22 +121,23 @@ impl Publish {
         object["txid"] = extern_id.into();
         object["order"] = order.to_json();
 
-        let topic = format!("settle.{}", order.user_id%USER_GROUP_SIZE);
+        let group_id = order.user_id%self.settle_group_count;
+        let topic = format!("settle.{}", group_id);
 
         info!("{} {}", topic, object);
 
-        let message = PublishInfo {
+        let message = SettlePublishTaskInfo {
+            group_id:group_id,
+            message_id:m.message_id,
             topic: topic,
             data: object,
         };
 
-        self.sender.send(Box::new(message)).expect("send failed");
+        self.sender.send(Box::new(OutputPublishTask::SettlePublishTask(message))).expect("send failed");
     }
 
-    pub fn publish_deal(&self, m: &mut Market, extern_id: u64, tm: i64, user_id: u32, rival_user_id: u32, order_id: u64, role: u32,
+    pub fn publish_deal(&self, m: &Market, extern_id: u64, tm: i64, user_id: u32, rival_user_id: u32, order_id: u64, role: u32,
             price: &Decimal, amount: &Decimal, deal: &Decimal, fee: &Decimal, rival_fee: &Decimal) {
-
-        m.message_id += 1;
 
         let mut object = JsonValue::new_object();
         object["type"] = "deals".into();
@@ -112,16 +160,20 @@ impl Publish {
 
         object["deals"] = deals;
 
-        let topic = format!("settle.{}", user_id%USER_GROUP_SIZE);
+
+        let group_id = user_id%self.settle_group_count;
+        let topic = format!("settle.{}", group_id);
 
         info!("{} {}", topic, object);
 
-        let message = PublishInfo {
+        let message = SettlePublishTaskInfo {
+            group_id:group_id,
+            message_id:m.message_id,
             topic: topic,
             data: object,
         };
 
-        self.sender.send(Box::new(message)).expect("send failed");
+        self.sender.send(Box::new(OutputPublishTask::SettlePublishTask(message))).expect("send failed");
     }
 
     pub fn publish_error(&self, m: &mut Market, extern_id: u64, user_id: u32, params: &JsonValue, code: u32) {
@@ -136,16 +188,19 @@ impl Publish {
         object["params"] = params.clone();
         object["code"] = code.into();
 
-        let topic = format!("settle.{}", user_id%USER_GROUP_SIZE);
+        let group_id = user_id%self.settle_group_count;
+        let topic = format!("settle.{}", group_id);
 
         info!("{} {}", topic, object);
 
-        let message = PublishInfo {
+        let message = SettlePublishTaskInfo {
+            group_id:group_id,
+            message_id:m.message_id,
             topic: topic,
             data: object,
         };
 
-        self.sender.send(Box::new(message)).expect("send failed");
+        self.sender.send(Box::new(OutputPublishTask::SettlePublishTask(message))).expect("send failed");
     }
 
     pub fn publish_quote_deal(&self, m: &Market, tm: i64, price: &Decimal, amount: &Decimal, side: u32) {
@@ -167,11 +222,12 @@ impl Publish {
 
         info!("{} {}", topic, object);
 
-        let message = PublishInfo {
+        let message = QuotePublishTaskInfo {
+            deals_id: m.deals_id,
             topic: topic,
             data: object,
         };
 
-        self.sender.send(Box::new(message)).expect("send failed");
+        self.sender.send(Box::new(OutputPublishTask::QuotePublishTask(message))).expect("send failed");
     }
 }
